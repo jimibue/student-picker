92b3879bbf462896094bf1bccd02fc0f5209a72a
James Yeates <jimibue@gmail.com>
2020-05-14 11:07:46 -0600

    stubed out comments for project

--- main.rb
+++ main.rb
@@ -1,1 +1,9 @@
-# make a change
+students = ["Leveille, Andre", "AshWorth, Ryan", "Biddinger, Caden", "Mbia, Arsene", "Boyden, Lindsay", "DelaCruz, Joey", "Dunshee, Kyle", "Riches, James", "Smith, Shawn", "Sultan, Maxwell", "Zarate, Javier"]
+
+# my task now is to create a group randomizer
+
+# create a new array of hashes with key fist_name and last_name
+
+# with the newly create array i want to randomly people and pull and assing into groups
+
+# print out groups in a slack friendly format
